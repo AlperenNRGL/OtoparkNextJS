2a5957b31287884c58ee7e77fce1b53aec909fa2
AlperenNRGL <alperennuroglu@hotmail.com>
2026-01-01 14:18:17 +0300

    Vercel Analytics Conf and Speed Added

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 export const metadata = {
   title: 'Blog App',
@@ -11,6 +12,7 @@ export default function RootLayout({ children }) {
       <body>
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
